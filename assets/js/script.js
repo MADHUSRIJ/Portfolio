6da6a77a60dbd4f92b0d4d9327e41b317ed4b82f
Madhusri J <srimadhu.j@gmail.com>
2023-05-23 10:30:07 +0530

    ADD. apply bootstrap

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,16 @@
-// Initial Script 
+function showDiv(divId) {
+    var buttons = document.getElementsByTagName('button');
+    for (var i = 0; i < buttons.length; i++) {
+        buttons[i].classList.remove('active-button');
+    }
+
+    var selectedButton = document.getElementById(divId + 'Button');
+    selectedButton.classList.add('active-button');
+
+    var divs = document.getElementsByClassName('basic-details');
+    for (var j = 0; j < divs.length; j++) {
+        divs[j].classList.add('d-none');
+    }
+    var selectedDiv = document.getElementById(divId);
+    selectedDiv.classList.remove('d-none');
+}
